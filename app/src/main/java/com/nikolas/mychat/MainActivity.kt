936b9685c747f396fef2698b00bc53c11f99b890
Bakulin Nikolas <nbakylin@gmail.com>
2022-03-15 20:51:03 +0300

    read database

--- app/src/main/java/com/nikolas/mychat/MainActivity.kt
+++ app/src/main/java/com/nikolas/mychat/MainActivity.kt
@@ -2,17 +2,39 @@ package com.nikolas.mychat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
+import com.nikolas.mychat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("messages");
+        val myRef = database.getReference("messages")
+        binding.bSend.setOnClickListener {
+            myRef.setValue(binding.edMessage.text.toString())
+        }
+        onChangeListener(myRef)
+    }
 
-        myRef.setValue("Hello, World!");
+    private fun onChangeListener(dRef: DatabaseReference) {
+        dRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                binding.apply {
+                    rcView.append("\n")
+                    rcView.append(snapshot.value.toString())
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
 }
