bfcf620469fadfc3b3ad4b66ce88994b0c0ac145
Bakulin Nikolas <nbakylin@gmail.com>
2022-03-17 21:48:37 +0300

    theme, actionBar, signOut

--- app/src/main/java/com/nikolas/mychat/MainActivity.kt
+++ app/src/main/java/com/nikolas/mychat/MainActivity.kt
@@ -1,25 +1,49 @@
 package com.nikolas.mychat
 
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
 import com.nikolas.mychat.databinding.ActivityMainBinding
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        auth = Firebase.auth
+        setUpActionBar()
         val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("messages")
         binding.bSend.setOnClickListener {
             myRef.setValue(binding.edMessage.text.toString())
         }
         onChangeListener(myRef)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.sing_out -> {
+                auth.signOut()
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun onChangeListener(dRef: DatabaseReference) {
@@ -36,5 +60,20 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    private fun setUpActionBar() {
+        val ab = supportActionBar
+        Thread{
+            val bMap = Picasso.get().load(auth.currentUser?.photoUrl).get()
+            val dIcon = BitmapDrawable(resources, bMap)
+            runOnUiThread {
+                ab?.setDisplayHomeAsUpEnabled(true)
+                ab?.setHomeAsUpIndicator(dIcon)
+                ab?.title = auth.currentUser?.displayName
+            }
+
+        }.start()
+
     }
 }
